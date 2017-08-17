e1af2ca22658abf0dbc9e9cbf2a8a4e8721edd53
Naresh-Narsing <nareshnarsing38@gmail.com>
2017-08-17 14:39:37 +0530

    update functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators} from '@angular/forms';
+// import {Http, HTTP_PROVIDERS} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+// const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+//   viewProviders: [HTTP_PROVIDERS],
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'mySolarizd';
+export class AppComponent implements OnInit{
+  results: string[]; 
+
   phone = [{
         id: 1,
         name: 'Rajesh'
@@ -20,5 +27,22 @@ export class AppComponent {
         id: 3,
         name: 'Ramesh'
   }];
+//   constructor(private http: Http){}
+  ngOnInit(): void{
+       // Make the HTTP request:
+//      this.http.get('/api/items').subscribe(data => {
+       // Read the result field from the JSON response.
+      //  this.results = data['results'];
+//      });       
+  }
+
   // console.log('phone',phone)
+  searchData(data,event){
+      event.preventDefault();
+      console.log(data.dataSearch)
+  }
 }
+
+interface ItemsResponse {
+      results: string[];
+    }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 
-import { MdButtonModule } from '@angular2-material/button';
-import { MdCardModule } from '@angular2-material/card';
-import { MdListModule } from '@angular2-material/list';
-import { MdIconModule } from '@angular2-material/icon';
+import { MaterialModule } from '@angular/material';
+import { MD_PLACEHOLDER_GLOBAL_OPTIONS } from '@angular/material'
+// import { MdButtonModule } from '@angular2-material/button';
+// import { MdCardModule } from '@angular2-material/card';
+// import { MdListModule } from '@angular2-material/list';
+// import { MdIconModule } from '@angular2-material/icon';
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { MdIconRegistry } from '@angular2-material/icon';
+import 'hammerjs';
 
 
 
 @NgModule({
   declarations: [ AppComponent ],
   imports: [ BrowserModule,
-             MdButtonModule,
-             MdCardModule,
-             MdListModule,
-             MdIconModule, 
+             FormsModule,
+             HttpClientModule,
+             MaterialModule,
+             NoopAnimationsModule,
              FlexLayoutModule
             ],
-  providers: [ MdIconRegistry ],
+  providers: [ {provide: MD_PLACEHOLDER_GLOBAL_OPTIONS, 
+                useValue: { float: 'always' }},
+                MdIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
